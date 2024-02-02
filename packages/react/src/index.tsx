e4713dfe8cb812ca4fa8c14d1c3d200840dd5ddc
fel1pe-r-s <1felipersilva1@gmail.com>
2024-02-02 20:13:48 -0300

    ✨ feat(add font roboto):
    Criado o arquivo preview-head.html para adição da fonte roboto ao storyBook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,32 @@
-import { colors } from "@ignite-ui/tokens";
+import { ComponentProps } from "react";
+import { styled } from "./styles";
 
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello</h1>;
-}
+export const Button = styled("button", {
+  fontFamily: "$default",
+  padding: "$4",
+  backgroundColor: "$ignite300",
+  borderRadius: "$sm",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+  cursor: "pointer",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "small",
+  },
+});
+
+export type ButtonProps = ComponentProps<typeof Button>;
